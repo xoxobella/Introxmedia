a67a0f52dec67be8bfe2623002db13071fe50060
xoxobella <kuldeepsinhjadeja876@gmail.com>
2024-11-14 11:12:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Nav from './Components/Navbar/Navbar'
+const App = () => {
+  return (
+    <>
+    <Nav />
+    </>
+  )
+}
+
+export default App
